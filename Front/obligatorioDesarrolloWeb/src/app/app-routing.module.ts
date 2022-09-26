5f9d3f7712a5ea9653a84d4b8b41343ace7781ec
SofiaPareja <88661638+SofiaPareja@users.noreply.github.com>
2022-09-26 10:44:10 -0300

    catalog component

--- Front/obligatorioDesarrolloWeb/src/app/app-routing.module.ts
+++ Front/obligatorioDesarrolloWeb/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogComponent } from './components/catalog/catalog.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'catalog', component: CatalogComponent}
 ];
 
 @NgModule({
--- Front/obligatorioDesarrolloWeb/src/app/app.module.ts
+++ Front/obligatorioDesarrolloWeb/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
+import { WindmillPartComponent } from './components/windmill-part/windmill-part.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CatalogComponent,
+    WindmillPartComponent
   ],
   imports: [
     BrowserModule,
