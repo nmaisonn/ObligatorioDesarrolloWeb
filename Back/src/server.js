85c08913cb5651d308237bbed327b9318fbfa42a
Sebastian Fripp <irubyxrubyx@gmail.com>
2022-10-24 19:29:04 -0300

    BD connection created

--- Back/src/server.js
+++ Back/src/server.js
@@ -1,5 +1,16 @@
 const path = require("path")
 const express = require("express")
+const MongoClient = require("mongodb").MongoClient
+let usersCollection
+
+MongoClient.connect('', (err, client) => {
+    if (err) {
+        return console.log(err)
+    }
+    console.log("Connected to Database")
+    const db = client.db('dbObligatorio')
+    usersCollection = db.collection("users")
+})
 
 const app = express()
 
@@ -11,25 +22,31 @@ const publicDirPath = path.join(__dirname, "../public")
 // Setup static directory
 app.use(express.static(publicDirPath))
 
-app.post("/addUser", (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: "You must provide an address"
-        })
-    }
+app.post("/test", async (req, res) => {
+    const insertResult = await usersCollection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
+    console.log('Inserted documents =>', insertResult);
+    res.send(insertResult)
 })
 
-app.get("/listUsers", (req, res) => {
-    
-})
+// app.post("/addUser", (req, res) => {
+//     if (!req.query.address) {
+//         return res.send({
+//             error: "You must provide an address"
+//         })
+//     }
+// })
 
-app.delete("/deleteUser", (req, res) => {
+// app.get("/listUsers", (req, res) => {
 
-})
+// })
 
-app.post("/modifyUser", (req, res) => {
+// app.delete("/deleteUser", (req, res) => {
 
-})
+// })
+
+// app.post("/modifyUser", (req, res) => {
+
+// })
 
 app.listen(port, () => {
     console.log("Server is up on port " + port)
