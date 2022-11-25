5a2899af538b5d5bc5ecb56f3bc8a727a4983d52
Sebastian Fripp <irubyxrubyx@gmail.com>
2022-11-25 20:39:59 -0300

    Img render fixed

--- Front/obligatorioDesarrolloWeb/src/app/components/windmill-part-creation/windmill-part-creation.component.ts
+++ Front/obligatorioDesarrolloWeb/src/app/components/windmill-part-creation/windmill-part-creation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { windmillPart } from 'src/app/windmillPart';
+import { ImagenesService } from 'src/app/services/imagenes.service';
+
 
 @Component({
   selector: 'app-windmill-part-creation',
@@ -9,10 +11,27 @@ import { windmillPart } from 'src/app/windmillPart';
 export class WindmillPartCreationComponent implements OnInit {
 
   @Input() coso: windmillPart | any;
+  imagen: any;
 
-  constructor() { }
+
+
+  constructor(private imagenService: ImagenesService) { }
 
   ngOnInit(): void {
+    this.imagenService.getImg(this.coso.img).subscribe((res)=>{
+     this.blobToImage(res);
+
+    })
+  }
+
+  blobToImage(theBlob : Blob){
+    let reader = new FileReader();
+    reader.addEventListener("load",() =>{
+      this.imagen = reader.result;
+    },false)
+    if(theBlob){
+      reader.readAsDataURL(theBlob);
+    }
   }
 
 }
